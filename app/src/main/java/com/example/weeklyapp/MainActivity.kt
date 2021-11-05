d2a738c6098168e13fd4aebbdb0d6985a785f673
Zarah Floro <znfloro@up.edu.ph>
2021-11-05 14:20:26 +0800

    Commit #3 Basic Navigation and Activities
    Added a Recycler View for display of future tasks.
    
    Added a Floating Action Button for starting Create Task Activity.
    
    Replaced Synthetic Binding with View Binding to eliminate risk of depreciation.
    
    Added text view in list_task_item for future Recycler View use.
    
    Replaced former fixed toolbar width and height to match_parent.
    
    Adding string resources for views.

--- app/src/main/java/com/example/weeklyapp/MainActivity.kt
+++ app/src/main/java/com/example/weeklyapp/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.weeklyapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
+import com.example.weeklyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        val toggle = ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close)
-        drawerLayout.addDrawerListener(toggle)
+        // bind views from activity_main
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // enables side navigation
+        setSupportActionBar(binding.contentMain.toolbar)
+        val toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            binding.contentMain.toolbar,
+            R.string.open,
+            R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
         toggle.isDrawerIndicatorEnabled = true
         toggle.syncState()
+
+        // on click listener to start Create Task Activity
+        binding.fab.setOnClickListener {
+            val intent = Intent(this, CreateTaskActivity::class.java)
+            startActivity(intent)
+        }
+
     }
+
 }
