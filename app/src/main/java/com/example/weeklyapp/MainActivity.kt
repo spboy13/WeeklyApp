fba6761be957acf04b31d757aa3c4fc786e014cd
ansem7 <dpramirez@up.edu.ph>
2021-11-05 13:15:02 +0800

    - side navigation

--- app/src/main/java/com/example/weeklyapp/MainActivity.kt
+++ app/src/main/java/com/example/weeklyapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.weeklyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.isDrawerIndicatorEnabled = true
+        toggle.syncState()
     }
 }
